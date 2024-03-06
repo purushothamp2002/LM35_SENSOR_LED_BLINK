35711ec9893e7352edda069936255aaa3234ae0a
purushothamp2002 <142765763+purushothamp2002@users.noreply.github.com>
2024-03-06 11:29:15 +0530

    Update TempSensor_Greendzine.ino

--- TempSensor_Greendzine.ino
+++ TempSensor_Greendzine.ino
@@ -1,12 +1,12 @@
 #include<elapsedMillis.h>
 
-const int lm35Pin = A0;
-//const int ledPin = 13;
+const int lm35Pin = A0; // analog pin to read analog reading from LM35 sensor
+
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);  /// Set the built-in LED pin as an output
 
 }
 
@@ -15,20 +15,21 @@ void loop() {
 
   uint8_t Temp_ADC_Val;
   float tempVal;
-  Temp_ADC_Val = analogRead(lm35Pin);
-  tempVal = (Temp_ADC_Val * 4.88) / 10;
+  Temp_ADC_Val = analogRead(lm35Pin); // Read analog voltage from LM35
+  tempVal = (Temp_ADC_Val * 4.88) / 10; /// Convert ADC value to temperature in Celsius
 
   if(tempVal < 30.0)
   {
-    BlinkLed(250);
+    BlinkLed(250);   // Blink LED every 250 milliseconds if temperature is below 30°C
   }
-  else if (tempVal >= 30.0) 
+  else if (tempVal >= 30.0)   // Blink LED every 500 milliseconds if temperature is 30°C or above
   {
     BlinkLed(500);
   }
 
 }
 
+// funtion to blink LED according to temperature
 void BlinkLed(int delay)
 {
   static elapsedMillis previousMillis = 0;
@@ -37,8 +38,8 @@ void BlinkLed(int delay)
   if(previousMillis >= delay)
   {
     previousMillis = 0;
-    LedState = !LedState;
-    digitalWrite(LED_BUILTIN,LedState);
+    LedState = !LedState;     // Toggle LED state
+    digitalWrite(LED_BUILTIN,LedState); // Set the built-in LED state
 
   }
 }
